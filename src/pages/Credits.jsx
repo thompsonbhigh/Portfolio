37f0e753cd3ee05ac5cdb223bd2b23c5d1f995a1
thompsonbhigh <thompsonbhigh@gmail.com>
2026-02-02 18:10:55 -0600

    styled credits page

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 
 const Credits = () => {
   return (
-    <main class='max-w-screen min-h-screen p-20 bg-[#493D9E]'>
-        <p class='text-2xl p-5 text-[#DAD2FF]'>
-            "Cosmonaut on a rocket" (https://skfb.ly/o6AyV) by Yury Misiyuk is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-        </p>
-        <p class='text-2xl p-5 text-[#DAD2FF]'>
-            "...Fire in the sky" (https://skfb.ly/oCXvT) by WON is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-        </p>
-        <p class='text-2xl p-5 text-[#DAD2FF]'>
-            "Deep Space skybox" (https://skfb.ly/puuOG) by Jungle Jim is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-        </p>
-        <p class='text-2xl p-5 text-[#DAD2FF]'>
-            "Chibi UFO - Low Poly" (https://skfb.ly/6WOnv) by dammne is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
-        </p>
+    <main class='about-main'>
+        <div class="credits-flex">
+            <div class="glass-card-credit">
+                <p>
+                    "Cosmonaut on a rocket" (https://skfb.ly/o6AyV) by Yury Misiyuk is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+                </p>
+            </div>
+            <div class="glass-card-credit">
+                <p>
+                    "...Fire in the sky" (https://skfb.ly/oCXvT) by WON is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+                </p>
+            </div>
+            <div class="glass-card-credit">
+                <p>
+                    "Deep Space skybox" (https://skfb.ly/puuOG) by Jungle Jim is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+                </p>
+            </div>
+            <div class="glass-card-credit">
+                <p>
+                    "Chibi UFO - Low Poly" (https://skfb.ly/6WOnv) by dammne is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+                </p>
+            </div>
+        </div>
     </main>
   )
 }
